b80853fc19ee48bc002aeeef8c439376b2578712
Omer Demirkan <omerfarukpiano@gmail.com>
2020-03-07 12:40:38 -0800

    Connect mongoose to Omer's mongoDB atlas

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
-
 const app = express();
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://admin-omer:1907Fbfb@cluster0-jgx0j.mongodb.net/ijdb?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    console.log('Connected to mongoDB atlas');
+})
+.catch(() => {
+    console.log('Error in connecting to mongoDB atlas');
+});
 
 const PORT = process.env.PORT || 5000;
 
