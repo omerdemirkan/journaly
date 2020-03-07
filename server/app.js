765010be1c24b4e3c4d6336cda707cf8a20b1bb9
Omer Demirkan <omerfarukpiano@gmail.com>
2020-03-07 13:05:44 -0800

    create journalist router with post and get method

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://admin-omer:1907Fbfb@cluster0-jgx0j.mongodb.net/ijdb?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
@@ -10,6 +12,10 @@ mongoose.connect('mongodb+srv://admin-omer:1907Fbfb@cluster0-jgx0j.mongodb.net/i
 .catch(() => {
     console.log('Error in connecting to mongoDB atlas');
 });
+
+const journalistRouter = require('./routes/journalist');
+
+app.use('/api/journalist', journalistRouter);
 
 const PORT = process.env.PORT || 5000;
 
--- server/routes/journalist.js
+++ server/routes/journalist.js
@@ -1,3 +1,29 @@
 const router = require('express').Router();
 
+const Journalist = require('../models/journalist');
+
+router.post('/', (req, res) => {
+    const journalist = req.body.journalist;
+
+    const newJournalist = new Journalist(journalist);
+
+    newJournalist.save(err => {
+        if (err) return res.status(400);
+
+        res.json('journalist successfully saved');
+    });
+});
+
+router.get('/', (req, res) => {
+    const journalistName = req.query.name;
+
+    Journalist.findOne({name: journalistName}, (err, foundJournalist) => {
+        if (err) return res.status(400);
+
+        if (foundJournalist) {
+            res.json(foundJournalist);
+        }
+    });
+});
+
 module.exports = router;
