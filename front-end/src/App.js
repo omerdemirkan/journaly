3943c67cc65d9c3b77dcdb5f44145e59fe79c87f
Omer Demirkan <omerfarukpiano@gmail.com>
2020-03-07 20:36:49 -0800

    create two empty page components with corresponding routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,23 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Route} from 'react-router-dom';
+
+// Pages
+
+import SignUp from './pages/SignUp/SignUp';
+import Rate from './pages/Rate/Rate';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path='/sign-up'/>
+      <Route path='/rate/:journalistId'/>
     </div>
   );
 }
