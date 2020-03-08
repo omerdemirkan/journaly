d35fd289707ba620f40a34d47399b92a3ec91a09
Shivay Lamba <shivaylamba@gmail.com>
2020-03-08 13:40:18 +0530

    fixed css

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import {Route} from 'react-router-dom';
 
